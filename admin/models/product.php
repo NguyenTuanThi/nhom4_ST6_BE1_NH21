d29c91a74756f8d0152b83540c82687d688069b3
voyen057 <voyen057@gmail.com>
2021-11-28 18:26:56 +0700

    yen add

--- admin/models/product.php
+++ admin/models/product.php
@@ -3,13 +3,27 @@ class Product extends Db
 {
     public function getAllProducts()
     {
-        $sql = self::$connection->prepare("SELECT * 
+        $sql = self::$connection->prepare(
+            "SELECT * 
         FROM `products`,`manufactures`,`protypes`
         WHERE `products`.`manu_id` = `manufactures`.`manu_id`
-        AND `products`.`type_id` = `protypes`.`type_id`");
+        AND `products`.`type_id` = `protypes`.`type_id`
+        ORDER by  `id` desc "
+        );
         $sql->execute(); //return an object
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items; //return an array
+    }
+    public function addProduct($name, $manu_id, $type_id, $price,  $image, $desc, $feature, $created_at)
+    {
+        $sql = self::$connection->prepare("INSERT 
+        INTO `products`(`name`,`manu_id`,`type_id`,`price`,`image`,`description`,`feature`,`created_at`)
+        VALUE (?,?,?,?,?,?,?,?)");
+        var_dump("INSERT 
+        INTO `products`(`name`,`manu_id`,`type_id`,`price`,`image`,`description`,`feature`,`created_at`)
+        VALUE ( $name, $manu_id, $type_id, $price,  $image,'$desc', $feature, $created_at)");
+        $sql->bind_param("siiissis", $name, $manu_id, $type_id, $price,  $image, $desc, $feature, $created_at);
+        return $sql->execute(); //return an object
     }
 }
